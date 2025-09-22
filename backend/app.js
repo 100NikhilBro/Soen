35153f75a6c6634085725417163908434cff9f77
Nikhil Gupta <100pranjalgupta@gmail.com>
2025-09-22 20:35:40 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const hpp = require("hpp");
+const helmet = require("helmet");
 
 const cors = require('cors');
 app.use(cors({
@@ -14,6 +15,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(hpp());
+app.use(helmet());
 
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
